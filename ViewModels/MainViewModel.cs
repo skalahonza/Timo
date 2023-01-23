13861b13f55804922993d776cbc72a34ad6291c7
Jan Skála <j.skala@hotmail.com>
2023-01-23 08:38:39 +0100

    Improve flow to avoid miss-clicks

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿namespace Timo.ViewModels;
+
+public class MainViewModel
+{
+    /// <summary>
+    /// Number of available cannulas.
+    /// </summary>
+    public int Cannulas { get; set; }
+    
+    /// <summary>
+    /// When was the last cannula change.
+    /// </summary>
+    public DateTime LastCannulaChange { get; set; }
+    
+    /// <summary>
+    /// Number of available sensors.
+    /// </summary>
+    public int Sensors { get; set; }
+    
+    /// <summary>
+    /// When was the last sensor change.
+    /// </summary>
+    public DateTime LastSensorChange { get; set; }
+    
+    /// <summary>
+    /// Insert a new cannula.
+    /// </summary>
+    public void InsertCannula()
+    {
+        Cannulas--;
+        LastCannulaChange = DateTime.Now;
+    }
+    
+    /// <summary>
+    /// Insert a new sensor.
+    /// </summary>
+    public void InsertSensor()
+    {
+        Sensors--;
+        LastSensorChange = DateTime.Now;
+    }
+}
